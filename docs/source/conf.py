d2224f95e9bcce3350b47c683be41980e7ae6c42
Alfred Santacatalina <alfred82santa@gmail.com>
2014-08-13 17:23:35 +0200

    PEP8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -192,22 +192,22 @@ htmlhelp_basename = 'DirtyFiltersdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'DirtyFilters.tex', 'Dirty Filters Documentation',
-   'alfred82santa', 'manual'),
+    ('index', 'DirtyFilters.tex', 'Dirty Filters Documentation',
+     'alfred82santa', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -250,9 +250,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'DirtyFilters', 'Dirty Filters Documentation',
-   'alfred82santa', 'DirtyFilters', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'DirtyFilters', 'Dirty Filters Documentation',
+     'alfred82santa', 'DirtyFilters', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
